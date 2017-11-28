6ced0929e51d2ed9f0f20d75a4ba48ef9a1fa20d
Marshall Houston <marshall.houston@gmail.com>
2017-11-27 20:01:31 -0700

    add test for visiting api/v1/merchants

--- spec/requests/api/v1/merchants/merchants_api_spec.rb
+++ spec/requests/api/v1/merchants/merchants_api_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 describe "Merchants API" do
   it "sends a collection of merchants" do
-    
+    create_list(:merchants, 3)
+
+    get "/api/v1/merchants"
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_success
+    expect(merchants.count).to eq(3)
   end
 end
