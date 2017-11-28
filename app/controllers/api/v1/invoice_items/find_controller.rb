696862e19a07eef47b3b60baaae81eaaf8faffc4
Liam Barstad <liambarstad@gmail.com>
2017-11-28 08:07:32 -0700

    add invoice_items tests/routes/controllers

---
+++ app/controllers/api/v1/invoice_items/find_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::InvoiceItems::FindController < ApplicationController
+
+    def show
+        binding.pry
+        render json: InvoiceItem.where(invoice_item_params).first
+    end
+
+    def index
+        render json: InvoiceItem.where(invoice_item_params)
+    end
+
+    private
+
+    def invoice_item_params
+        params.permit(:id, :quantity, :unit_price, :created_at, :updated_at)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
+
       get '/invoices/find', to: 'invoices/find#show'
       get '/invoices/find_all', to: 'invoices/find#index'
       get '/invoices/random', to: 'invoices/random#show'
+
+      get '/invoice_items/find', to: 'invoice_items/find#show'
+      get '/invoice_items/find_all', to: 'invoice_items/find#index'
+      get '/invoice_items/random', to: 'invoice_items/random#show'
 
       resources :invoices, only: [:index, :show]
       resources :items, only: [:index, :show]
