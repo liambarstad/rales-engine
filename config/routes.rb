ffa916a1dbafc4cfce8cb19e1115ae1ebe546c82
Liam Barstad <liambarstad@gmail.com>
2017-11-28 16:19:55 -0700

    pass rspec find/find_all/random tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
+      namespace :items do
+        get '/find', to: 'find#show'
+        get '/find_all', to: 'find#index'
+        get '/random', to: 'random#show'
+      end
 
       get '/invoices/find', to: 'invoices/find#show'
       get '/invoices/find_all', to: 'invoices/find#index'
