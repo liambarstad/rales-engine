135bfc277a513cd1d996ef15d7ac24a6962fe468
Liam Barstad <liambarstad@gmail.com>
2017-11-28 15:07:06 -0700

    add model relationship testing

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:transactions) }
+  it { should have_many(:invoice_items) }
+  it { should have_many(:items) }
+  it { should belong_to(:merchant) }
+  it { should belong_to(:customer) }
 end
