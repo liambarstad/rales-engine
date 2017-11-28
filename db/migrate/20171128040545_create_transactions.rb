f156746913f8b132701e5c5d9db3dc602e4a6b15
Marshall Houston <marshall.houston@gmail.com>
2017-11-27 21:20:08 -0700

    change credit_card_number to string, remove created_at/updated_at columns

--- db/migrate/20171128040545_create_transactions.rb
+++ db/migrate/20171128040545_create_transactions.rb
@@ -2,10 +2,8 @@ class CreateTransactions < ActiveRecord::Migration[5.1]
   def change
     create_table :transactions do |t|
       t.integer :invoice_id
-      t.integer :credit_card_number
+      t.string :credit_card_number
       t.string :result
-      t.datetime :created_at
-      t.datetime :updated_at
 
       t.timestamps
     end
