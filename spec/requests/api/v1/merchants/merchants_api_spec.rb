d04f6386a1bee2ec1633dca39f3758c7ea199185
Marshall Houston <marshall.houston@gmail.com>
2017-11-27 20:43:26 -0700

    small update to test

--- spec/requests/api/v1/merchants/merchants_api_spec.rb
+++ spec/requests/api/v1/merchants/merchants_api_spec.rb
@@ -13,15 +13,14 @@ describe "Merchants API" do
   end
 
   it "returns a single merchant" do
-    merchants = create_list(:merchant, 2)
-    merchant = merchants.first
+    merchant1, merchant2 = create_list(:merchant, 2)
 
-    get "/api/v1/merchants/#{merchant.id}"
+    get "/api/v1/merchants/#{merchant1.id}"
 
     parsed_merchant = JSON.parse(response.body, symbolize_names: true)
     expect(response).to be_success
     expect(parsed_merchant).to have_key(:name)
-    expect(parsed_merchant[:id]).to eq(merchant.id)
-    expect(parsed_merchant[:name]).to_not eq(merchants.last.name)
+    expect(parsed_merchant[:id]).to eq(merchant1.id)
+    expect(parsed_merchant[:name]).to_not eq(merchant2.name)
   end
 end
