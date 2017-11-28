c24e2d6da168f79c70aac5ebfd20867a856bb188
Marshall Houston <marshall.houston@gmail.com>
2017-11-27 21:29:35 -0700

    add failing test for single transaction request

--- spec/requests/api/v1/transactions/transactions_api_spec.rb
+++ spec/requests/api/v1/transactions/transactions_api_spec.rb
@@ -12,4 +12,18 @@ describe "Transactions API" do
     expect(response.status).to eq(200)
     expect(transactions.count).to eq(4)
   end
+
+  it "returns a single transaction" do
+    transaction1, transaction2 = create_list(:transaction, 2)
+
+    get "/api/v1/transactions/#{transaction1.id}.json"
+
+    parsed_transaction1 = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response.status).to eq(200)
+    expect(parsed_transaction1).to have_key(:result)
+    expect(parsed_transaction1).to have_key(:invoice_id)
+    expect(parsed_transaction1[:id]).to eq(transaction1.id)
+    expect(parsed_transaction1[:id]).to_not eq(transaction2.id)
+  end
 end
