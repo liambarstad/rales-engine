2c2cfa5692309ab6393ea553226c0edb5ce99ea4
Marshall Houston <marshall.houston@gmail.com>
2017-11-27 20:08:33 -0700

    change merchant factory to singular name

--- spec/requests/api/v1/merchants/merchants_api_spec.rb
+++ spec/requests/api/v1/merchants/merchants_api_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "Merchants API" do
   it "sends a collection of merchants" do
-    create_list(:merchants, 3)
+    create_list(:merchant, 3)
 
     get "/api/v1/merchants"
 
