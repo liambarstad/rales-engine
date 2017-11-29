c347dc185fbeea75fd6cb3efb02e6aafd51365a6
Marshall Houston <marshall.houston@gmail.com>
2017-11-29 07:45:19 -0700

    change status column to citext on invoices

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171129144224) do
+ActiveRecord::Schema.define(version: 20171129144402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 20171129144224) do
   end
 
   create_table "invoices", force: :cascade do |t|
-    t.string "status"
+    t.citext "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "merchant_id"
