340d74545120a18f5a765b9c4dc925466b485cda
Liam Barstad <liambarstad@gmail.com>
2017-11-29 13:33:36 -0700

    add transaction scopes :successful and :unsuccessful

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,14 @@
 class Merchant < ApplicationRecord
+
   has_many :items
   has_many :invoices
+
+  def favorite_customer
+    Customer.left_joins(invoices: :transactions)
+    .group('customers.id')
+    .where(invoices: { merchant_id: id }, transactions: { result: 'success' })
+    .order('count(transactions.id)')
+    .first
+  end
+
 end
