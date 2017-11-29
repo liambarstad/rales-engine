97353e2cdeb6762620effa3df9ea9a17a81ec153
Marshall Houston <marshall.houston@gmail.com>
2017-11-29 07:47:06 -0700

    change name column to citext on items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171129144402) do
+ActiveRecord::Schema.define(version: 20171129144611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 20171129144402) do
   end
 
   create_table "items", force: :cascade do |t|
-    t.string "name"
+    t.citext "name"
     t.float "unit_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
