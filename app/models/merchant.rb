a3cbd47a21962625055be06812db5c0a533868ad
Marshall Houston <marshall.houston@gmail.com>
2017-11-29 14:54:45 -0700

    resolve merge conflict

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,6 +3,7 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
 
+
   def favorite_customer
     Customer.left_joins(invoices: :transactions)
     .group('customers.id')
@@ -10,6 +11,13 @@ class Merchant < ApplicationRecord
     .merge(Transaction.successful)
     .order('count(transactions.id) DESC')
     .first
+  end
+
+  def revenue
+    invoices
+    .joins(:transactions, :invoice_items)
+    .where(transactions: {result: 'success'})
+    .sum('invoice_items.quantity * invoice_items.unit_price')
   end
 
 end
