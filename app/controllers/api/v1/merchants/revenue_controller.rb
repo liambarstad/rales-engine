cf7dd9718ab00ad0f2f23dd319e66d839c1ebd20
Liam Barstad <liambarstad@gmail.com>
2017-11-29 17:25:34 -0700

    add passing revenue without date

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
 
   def index
-    render json: Merchant.total_revenue
+    render json: { revenue: Merchant.total_revenue.to_s }
   end
 
 end
