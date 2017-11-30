4476b64049c9a7066f608d3a17907f79088d3042
Marshall Houston <marshall.houston@gmail.com>
2017-11-30 15:18:31 -0700

    failing test for customer.favorite_merchant

---
+++ spec/requests/api/v1/customers/favorite_merchant_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe "Customers API" do
+  describe "GET /api/v1/customers/:id/favorite_merchant" do
+    it 'calls favorite_merchant on customer and returns the favorite merchant' do
+
+      customer = double()
+      merchant = create :merchant
+
+      allow(Customer).to receive(:find).with('1') { customer }
+      expect(customer).to receive(:favorite_merchant).once
+
+      get '/api/v1/customers/1/favorite_merchant'
+
+      parsed_favorite_merchant = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(parsed_favorite_merchant['id']).to eq merchant.id
+      expect(parsed_favorite_merchant['name']).to eq merchant.name
+    end
+  end
+end
