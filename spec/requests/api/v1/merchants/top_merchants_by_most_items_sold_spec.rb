035b803c3c652ef194347c1f547cb56faac73229
Marshall Houston <marshall.houston@gmail.com>
2017-11-30 14:12:30 -0700

    update controller test

--- spec/requests/api/v1/merchants/top_merchants_by_most_items_sold_spec.rb
+++ spec/requests/api/v1/merchants/top_merchants_by_most_items_sold_spec.rb
@@ -4,8 +4,8 @@ describe "Merchants API" do
   describe "get /api/v1/merchants/most_items?quantity=x" do
     it "calls most_items on Merchant with quantity=x from params and returns top x merchants" do
       merchants = create_list(:merchant, 3)
-      quantity_params = "3"
-      expect(Merchant).to receive(:most_items).with(quantity_params).once { merchants }
+
+      expect(Merchant).to receive(:most_items).with("3").once { merchants }
 
       get "/api/v1/merchants/most_items?quantity=3"
 
