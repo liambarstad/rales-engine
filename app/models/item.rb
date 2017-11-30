171b61badd396ccd26c881b9c7cff4866ca5d9e0
Liam Barstad <liambarstad@gmail.com>
2017-11-30 09:36:48 -0700

    4 tests left

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,18 @@ class Item < ApplicationRecord
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
 
+  before_create :format_unit_price
+
   def self.most_sold_items(quantity = 5)
     select('items.*, sum(invoice_items.quantity) as total_sold')
     .joins(:invoices)
     .group(:id)
     .order('total_sold desc').limit(quantity)
+  end
+
+  private
+
+  def format_unit_price
+    self.unit_price = (self.unit_price.to_f / 100).round(2)
   end
 end
