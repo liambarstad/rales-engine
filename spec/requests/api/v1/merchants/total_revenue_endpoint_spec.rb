db2b06f978f53193163a60b2dce15e813469e866
Liam Barstad <liambarstad@gmail.com>
2017-11-29 19:20:34 -0700

    add date to model, controller, tests

--- spec/requests/api/v1/merchants/total_revenue_endpoint_spec.rb
+++ spec/requests/api/v1/merchants/total_revenue_endpoint_spec.rb
@@ -1,7 +1,8 @@
+require 'date'
 require 'rails_helper'
 
 RSpec.describe "can return the total revenue across all merchants" do
-  it "with no date" do
+  it "with date" do
     invoice1, invoice2, invoice3 = create_list(:invoice, 3)
     item = create(:item)
     invoice_item1 = create(:invoice_item, invoice: invoice1, item: item, quantity: 4, unit_price: 500)
@@ -9,18 +10,16 @@ RSpec.describe "can return the total revenue across all merchants" do
     invoice_item3 = create(:invoice_item, invoice: invoice3, item: item, quantity: 10, unit_price: 400)
     transaction1 = create(:transaction, invoice: invoice1, result: 'success')
     transaction2 = create(:transaction, invoice: invoice2, result: 'success')
-    transaction3 = create(:transaction, invoice: invoice3, result: 'success')
+    transaction3 = create(:transaction, invoice: invoice3, result: 'success', created_at: DateTime.now)
 
     total1 = invoice_item1.quantity * invoice_item1.unit_price
     total2 = invoice_item2.quantity * invoice_item2.unit_price
-    total3 = invoice_item3.quantity * invoice_item3.unit_price
-    total = total1 + total2 + total3
+    total = total1 + total2
     
-    get '/api/v1/merchants/revenue'
+    get '/api/v1/merchants/revenue?date=2012-03-27T14:54:05.000Z'
     result = JSON.parse(response.body)
     
     expect(response).to be_success
     expect(result["revenue"]).to eq(total.to_s)
   end
-
 end
