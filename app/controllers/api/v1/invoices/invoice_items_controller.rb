fd2b793421cb6c3fcb3bbf6568e941148ddccc91
Liam Barstad <liambarstad@gmail.com>
2017-11-30 15:30:07 -0700

    add invoice.invoice_items relationship endpoint

---
+++ app/controllers/api/v1/invoices/invoice_items_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::Invoices::InvoiceItemsController < ApplicationController
+
+  def index
+    invoice = Invoice.find(params["id"])
+    render json: invoice.invoice_items
+  end
+
+end
