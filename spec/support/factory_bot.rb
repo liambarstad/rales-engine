1f5af7ae10f509179750d882ec97624ff27b7970
Marshall Houston <marshall.houston@gmail.com>
2017-11-29 17:26:55 -0700

    add database cleaner setup

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,3 +1,15 @@
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  begin
+    DatabaseCleaner.start
+    FactoryBot.lint
+  ensure
+    DatabaseCleaner.clean
+  end
 end
