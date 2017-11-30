fefaada49f6542b96532575512af007febe76c76
Liam Barstad <liambarstad@gmail.com>
2017-11-30 16:34:22 -0700

    fix merge conflicts

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,7 @@
 class Customer < ApplicationRecord
   has_many :invoices
   has_many :merchants, through: :invoices
+  has_many :transactions, through: :invoices
 
   def favorite_merchant
     merchants
