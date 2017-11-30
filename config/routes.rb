012b71278f3c80582639c915924603953c123094
Liam Barstad <liambarstad@gmail.com>
2017-11-30 15:40:57 -0700

    add invoices.items endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
         get '/random', to: 'random#show'
         get '/:id/transactions', to: 'transactions#index'
         get '/:id/invoice_items', to: 'invoice_items#index'
+        get '/:id/items', to: 'items#index'
       end
 
       namespace :invoice_items do
