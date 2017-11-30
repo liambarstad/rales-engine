af77329b1f151395e90db26ff39eed2ca024b7e2
Liam Barstad <liambarstad@gmail.com>
2017-11-30 16:32:01 -0700

    add customer.transactions relationship endpoints

---
+++ app/controllers/api/v1/customers/transactions_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::Customers::TransactionsController < ApplicationController
+
+  def index
+    customer = Customer.find(params["id"])
+    render json: customer.transactions
+  end
+
+end
