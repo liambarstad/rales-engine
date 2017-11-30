a50a3584945a9d28d82119ab43e8caa549b79fca
Marshall Houston <marshall.houston@gmail.com>
2017-11-29 19:56:35 -0700

    add date requirement to failing test

--- spec/requests/api/v1/items/items_best_day_spec.rb
+++ spec/requests/api/v1/items/items_best_day_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'date'
 
 # assert_equal "2012-03-22T03:55:09.000Z", best_day_one["best_day"]
 # assert_equal "2012-03-20T23:57:05.000Z", best_day_two["best_day"]
@@ -8,10 +9,10 @@ describe "Items API" do
     it "returns the date with the most sold item according to invoice date" do
       merchant = create(:merchant)
       item1 = create(:item, name: "Item1", unit_price: 10000)
-      merchant.items << [item1, item2, item3]
-      invoice1 = create(:invoice, status: 'shipped', merchant: merchant, created_at: Datetime.now)
-      invoice2 = create(:invoice, status: 'shipped', merchant: merchant, created_at: Datetime.now)
-      invoice3 = create(:invoice, status: 'shipped', merchant: merchant, created_at: (Datetime.now - 1))
+      merchant.items << item1
+      invoice1 = create(:invoice, status: 'shipped', merchant: merchant, created_at: DateTime.now)
+      invoice2 = create(:invoice, status: 'shipped', merchant: merchant, created_at: DateTime.now)
+      invoice3 = create(:invoice, status: 'shipped', merchant: merchant, created_at: (DateTime.now - 1))
       item1.invoice_items << create(:invoice_item, invoice: invoice1, quantity: 4, unit_price: item1.unit_price)
       item1.invoice_items << create(:invoice_item, invoice: invoice2, quantity: 5, unit_price: item1.unit_price)
       item1.invoice_items << create(:invoice_item, invoice: invoice2, quantity: 5, unit_price: item1.unit_price)
