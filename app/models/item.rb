111dbe4a8baefd0634a14fdb926acbabca305d01
Marshall Houston <marshall.houston@gmail.com>
2017-11-29 18:30:26 -0700

    add passing test for items/most_sold?quantity=x

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,11 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  def self.most_sold_items(quantity = 5)
+    select('items.*, sum(invoice_items.quantity) as total_sold')
+    .joins(:invoices)
+    .group(:id)
+    .order('total_sold desc').limit(quantity)
+  end
 end
