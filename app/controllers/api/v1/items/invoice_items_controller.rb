6fa93621f0ee54590570a973b7eb413801942913
Marshall Houston <marshall.houston@gmail.com>
2017-11-30 21:25:49 -0700

    add relationship endpoints for items - invoice_items, merchant

---
+++ app/controllers/api/v1/items/invoice_items_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::Items::InvoiceItemsController < ApplicationController
+
+  def index
+    render json: item.invoice_items
+  end
+
+  private
+
+  def item
+    Item.find(params[:id])
+  end
+end
