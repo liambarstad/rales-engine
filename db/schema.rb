c142944eee676ce3d56f1f30ccb3183587c81609
Marshall Houston <marshall.houston@gmail.com>
2017-12-01 18:22:01 -0700

    run migrations that updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171129145031) do
+ActiveRecord::Schema.define(version: 20171202011559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20171129145031) do
     t.citext "result"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "credit_card_expiration_date"
   end
 
   add_foreign_key "invoice_items", "invoices"
